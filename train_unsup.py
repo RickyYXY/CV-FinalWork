076aead6b7860852a274bda3588792bbd73a741a
MaoMao <1459786819@qq.com>
2023-02-24 11:05:30 +0800

    epision

--- train_unsup.py
+++ train_unsup.py
@@ -90,7 +90,7 @@ def count_parameters(model):
 def fetch_optimizer(args, model):
     """ Create the optimizer and learning rate scheduler """
     optimizer = optim.AdamW(model.parameters(), lr=args.lr, amsgrad=True,
-                            weight_decay=args.wdecay)
+                            weight_decay=args.wdecay,eps=args.epsilon)
 
     scheduler = optim.lr_scheduler.OneCycleLR(optimizer, args.lr, args.num_steps+100,
                                                pct_start=0.05, cycle_momentum=False, anneal_strategy='linear')
